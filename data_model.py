419bf5711180ff53b891055de53ac1e5e03ea383
samuele.bragantini <samuele.bragantini@nokia.com>
2025-09-12 10:36:20 +0200

    feat: first endpoint completed

--- data_model.py
+++ data_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, model_validator
 from typing import List
 
 
@@ -7,10 +7,10 @@ class Tile(BaseModel):
     y: int
     walkable: bool
 
-    @field_validator("walkable")
-    def check_tiles(cls, walkable):
-        if walkable != 'O' and walkable != 'X':
-            raise ValueError("walkable must be either 'O' or 'X'")
+    # @field_validator("walkable")
+    # def check_tiles(cls, walkable):
+    #     if walkable != 'O' and walkable != 'X':
+    #         raise ValueError("walkable must be either 'O' or 'X'")
 
 
 class MapInput(BaseModel):
@@ -18,13 +18,43 @@ class MapInput(BaseModel):
     cols: int
     tiles: List[Tile]
 
-    @field_validator("cols")
-    def check_square(cls, v, values):
-        if "rows" in values and values["rows"] != v:
+    @model_validator(mode="after")
+    def check_square(self):
+        if self.rows != self.cols:
             raise ValueError("Map must be squared")
-        return v
+        return self
 
     @field_validator("tiles")
+    @classmethod
     def check_tiles(cls, tiles):
         if len(tiles) < 2:
             raise ValueError("Map must be walkable")
+
+
+def parse_map(text: str):
+    lines = text.strip().splitlines()
+    rows = len(lines)
+    cols = len(lines[0]) if len(lines) > 0 else 0
+
+    if rows == 0 or cols == 0:
+        raise ValueError('Empty map')
+    if rows == 1 and cols == 1:
+        raise ValueError('Non walkable map')
+    if rows != cols:
+        raise ValueError('Map must be squared')
+
+    data = {
+        "rows": rows,
+        "cols": cols,
+        "tiles": []
+    }
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+
+            if char.capitalize() != 'O' and char.capitalize() != 'X':
+                raise ValueError('Map cells must be O or X')
+            data['tiles'].append(
+                {"x": x, "y": y, "walkable": True if char == "O" else False}
+            )
+
+    return data
--- main.py
+++ main.py
@@ -1,85 +1,62 @@
-from fastapi import FastAPI, Depends, UploadFile, Body, File
+import json
+
+from fastapi import FastAPI, Depends, UploadFile, Body, File, HTTPException, Form
 from sqlalchemy.orm import Session
 
-from data_model import MapInput
+from data_model import MapInput, parse_map
 from db import get_db, Map
 
-# --- FastAPI ---
 app = FastAPI()
 
 
-def parse_map(text: str):
-    lines = text.strip().splitlines()
-    rows = len(lines)
-    cols = len(lines[0]) if len(lines) > 0 else 0
-
-    if rows == 0 or cols == 0:
-        raise ValueError('Empty map')
-    if rows == 1 and cols == 1:
-        raise ValueError('Non walkable map')
-    if rows != cols:
-        raise ValueError('Map must be squared')
-
-    data = {
-        "rows": rows,
-        "columns": cols,
-        "tiles": []
-    }
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-
-            if char != 'O' and char != 'X':
-                raise ValueError('Map cells must be O or X')
-            data['tiles'].append(
-                {"x": x, "y": y, "walkable": True if char == "O" else False}
-            )
-
-    return data
-
-
 @app.post("/set-map/")
-def set_map(id: int,
-            json_data: MapInput | None = Body(default=None),
-            file: UploadFile | None = File(default=None),
-            db: Session = Depends(get_db)):
-    if json_data:
-        data = json_data.dict()
-    elif file:
-        content = file.read().__str__()
-        data = parse_map(content)
-    else:
-        raise ValueError(f"Format {format} not supported")
+async def set_map(id: int,
+                  json_data: str | None = Form(default=None),
+                  file: UploadFile | None = File(default=None),
+                  db: Session = Depends(get_db)):
+    try:
+        if json_data:
+            parsed = json.loads(json_data)
 
-    map = Map(id=id, data=data)
-    db.add(map)
-    db.commit()
-    db.refresh(map)
-    return map
+            validated = MapInput(**parsed)
+            data = validated.dict()
+        elif file:
+            content = await file.read()
+            text = content.decode("utf-8")
+
+            parsed = parse_map(text)
+            validated = MapInput(**parsed)
+            data = validated.dict()
+
+        else:
+            raise ValueError(f"Format {format} not supported")
+
+        map = db.query(Map).filter(Map.id == id).first()
+
+        if map:
+            map.data = data
+        else:
+            map = Map(id=id, data=data)
+            db.add(map)
+
+        db.commit()
+        db.refresh(map)
+        return map
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 # Rotta: aggiungi utente
 @app.post("/clean/")
 def clean(nome: str, email: str, db: Session = Depends(get_db)):
-    user = User(nome=nome, email=email)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+    pass
 
 
 @app.get("/history/")
 def clean(nome: str, email: str, db: Session = Depends(get_db)):
-    user = User(nome=nome, email=email)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+    pass
 
 
 @app.get("/extended-functionality/")
 def clean(nome: str, email: str, db: Session = Depends(get_db)):
-    user = User(nome=nome, email=email)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+    pass
